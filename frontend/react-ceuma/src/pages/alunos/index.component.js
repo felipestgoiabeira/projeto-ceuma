70cd03e86cc3471adcbd3d338206ec4f7e2a24b5
Felipesg12 <felipesg.12@outlook.com>
2019-10-12 19:08:37 -0300

    alterando visual

--- frontend/react-ceuma/src/pages/alunos/index.component.js
+++ frontend/react-ceuma/src/pages/alunos/index.component.js
@@ -47,7 +47,7 @@ export default class AlunosListar extends Component {
     
     <div className="tb" >
       <h3>Tabela de Alunos</h3>
-      <table className="table table-striped" style={{ paddingTop: 20 }}>
+      <table className="table table-bordered" style={{ paddingTop: 20 }}>
         <thead className="thead-light">
           <tr>
             <th scope="col">Nome</th>
